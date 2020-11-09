c5c94b0d373b0bbe1e155116c547727f9f78a56a
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2020-11-09 10:20:02 +0530

    Add files via upload

---
+++ notepad.js
@@ -0,0 +1,18 @@
+    var editorKey = 'html5-notepad';
+    var editor = document.getElementById('editor');
+    var cache = localStorage.getItem(editorKey);
+
+    if (cache) {
+        editor.innerHTML = cache;
+        // editor.innerHTML = marked(cache);
+    }
+
+    function autosave() {
+        var newValue = editor.innerHTML;
+        if (cache != newValue) {
+            cache = newValue;
+            localStorage.setItem(editorKey, cache);
+        }
+    }
+
+    editor.addEventListener('input', autosave);
